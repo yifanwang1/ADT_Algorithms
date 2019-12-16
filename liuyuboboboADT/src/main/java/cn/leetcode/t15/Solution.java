5da567b6e6f04b93a45cf165f1383e133bc31145
wangyifan <964913576@qq.com>
2019-12-16 23:54:10 +0800

    20191216

--- liuyuboboboADT/src/main/java/cn/leetcode/t15/Solution.java
+++ liuyuboboboADT/src/main/java/cn/leetcode/t15/Solution.java
@@ -18,7 +18,8 @@ public class Solution {
         //int[] nums = new int[]{-1,0,1,2,-1,-4};
         //int[] nums = new int[]{0,0,0,0,0,0};
         int[] nums = new int[]{-2,0,1,1,2};
-        List<List<Integer>> resultList = s1.threeSum(nums);
+       // List<List<Integer>> resultList = s1.threeSum(nums);
+        List<List<Integer>> resultList = s1.threeSum2(nums);
         for (List i:resultList) {
             System.out.println(i.toString());
         }
@@ -43,13 +44,47 @@ public class Solution {
                 }
                 if (nums[start] + nums[end] == findValue) {
                     resultList.add(Arrays.asList(nums[i], nums[start], nums[end]));
-                    while (start == end ||(start != end && nums[start] == nums[start + 1])){
-                        start++;
-                    }
+                    //start = end ;
+                    start++ ;
                     continue;
                 }
                 if(nums[start] + nums[end] > findValue){
                     end--;
+                }
+            }
+        }
+        return resultList;
+    }
+
+
+    public List<List<Integer>> threeSum2(int[] nums) {
+
+        List<List<Integer>> resultList = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int findValue = 0 - nums[i];
+
+            int start = i + 1;
+            int end = nums.length - 1;
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+
+            for (int j = start; j <= end;) {
+                if(nums[start] > findValue){
+                    break;
+                }
+
+                if(nums[start] + nums[end] > findValue){
+                    end--;
+                    continue;
+                }
+
+                if(nums[start] + nums[end] == findValue){
+                    resultList.add(Arrays.asList(nums[i], nums[start], nums[end]));
+                    while(start < end && nums[start] == nums[start+1]){
+                        start++;
+                    }
                 }
             }
         }
