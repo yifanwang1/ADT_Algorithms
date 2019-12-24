d0307662f2f2937337572d33781d4ca0fa19258d
wangyifan <964913576@qq.com>
2019-12-24 22:33:37 +0800

    目录调整

--- liuyuboboboADT/src/main/java/com/liuyubobobo/list/ListNormal.java
+++ liuyuboboboADT/src/main/java/com/liuyubobobo/list/ListNormal.java
@@ -15,6 +15,12 @@ public class ListNormal<E> {
     private Node head;
     private int size;
 
+    public ListNormal(){
+        size = 0;
+        head = null;
+    }
+
+
     class Node{
         Node next;
         E e;
@@ -32,6 +38,23 @@ public class ListNormal<E> {
         }
     }
 
+    /**
+    * 返回链表的长度
+    **/
+    public int getSize(){
+        return size;
+    }
+
+    /**
+    * 判断链表是否为空
+    **/
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    /**
+    * 添加数据到链表中
+    **/
     public void add(int index, E e) throws Exception {
 
         if(index > size){
@@ -41,24 +64,45 @@ public class ListNormal<E> {
         if(index == 0){
             addFirst(e);
         }else{
-
             Node preNode = head;
-            for (int i = 0; i < index; i++) {
+            for (int i = 0; i < index - 1; i++) {
                 preNode = preNode.next;
             }
-
             Node node = new Node(e,preNode.next);
             preNode.next = node;
-
-
+            size++;
         }
     }
 
+    /**
+    * 添加到头结点
+    **/
     public void addFirst(E e){
         Node node = new Node(e, head);
         head = node;
+        size++;
     }
 
+    /**
+    * 删除指定位置的元素
+    **/
+    public E remove(int index){
+        Node currentNode = head;
+        for (int i = 0; i < size; i++) {
+            
+        }
 
+        return null;
+    }
 
+    @Override
+    public String toString(){
+        StringBuffer stringBuffer = new StringBuffer();
+        Node currentNode = head;
+        for (int i = 0; i < size; i++) {
+            stringBuffer.append(currentNode.e).append("\n");
+            currentNode = currentNode.next;
+        }
+        return stringBuffer.toString();
+    }
 }
